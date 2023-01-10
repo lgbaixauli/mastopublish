af967f1816bb1f79831717ea9c53b7b7afeae33b
lgbaixauli <lgbaixauli@gmail.com>
2023-01-10 11:17:07 +0100

    [202301101116]

--- publish.py
+++ publish.py
@@ -9,6 +9,7 @@ from pybot.logger import Logger
 import random
 
 BOT_NAME = "Terrybot"
+MAX_LENGHT = 490
 
 class Bot(Mastobot):
 
@@ -20,21 +21,21 @@ class Bot(Mastobot):
 
     def run(self, botname: str = BOT_NAME) -> None:
 
-        action   = self._actions["Publish_Terry"]
-        quote    = random.choice(action["quotes"])
+        quote    = random.choice(self._actions.get("Publish_Terry.quotes"))
         langauge = "en" 
  
-        self.post_toot (self.find_text(quote), langauge, quote["id"])
+        self.post_toot (self.find_text(quote), langauge)
 
         super().run(botname = botname)
 
 
     def find_text(self, quote):        
     
-        text     = quote["text"]                    
-        comments = quote["comments"]                    
-        source   = quote["source"]   
+        text     = quote[1]                    
+        comments = quote[2]                    
+        source   = quote[3]   
 
+        self._logger.debug("id      : " + str(quote[0]))                    
         self._logger.debug("text    : " + text)                    
         self._logger.debug("comments: " + comments)                     
         self._logger.debug("source  : " + source)   
@@ -42,10 +43,12 @@ class Bot(Mastobot):
         post_text  = text + "\n\n"
         if comments: post_text += comments + "\n"
         post_text += source + "\n\n"
-        post_text += "#GNUTerryPratchett, #SpeakHisName, #Discworld" 
 
-        post_text = (post_text[:400] + '... ') if len(post_text) > 400 else post_text
+        hashtag  = "#GNUTerryPratchett, #SpeakHisName, #Discworld" 
+        if len(post_text) + len(hashtag) < MAX_LENGHT:
+            post_text += hashtag
 
+        post_text = (post_text[:MAX_LENGHT] + '... ') if len(post_text) > MAX_LENGHT else post_text
         self._logger.debug ("answer text\n" + post_text)
 
         return post_text
